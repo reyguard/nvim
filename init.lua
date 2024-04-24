0456b9bd5550bb111a3b55d73a2056289adf36c9
Albin Rejgård <albin.rejgard@protonmail.com>
2024-04-24 15:55:01 +0200

    fixed a mistake on custom.plugins.init

--- init.lua
+++ init.lua
@@ -88,8 +88,8 @@ if vim.fn.has 'wsl' == 1 then
       ['*'] = 'clip.exe',
     },
     paste = {
-      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['+'] = 'win32yank.exe -o --lf',
+      ['*'] = 'win32yank.exe -o --lf',
     },
     cache_enabled = 0,
   }
